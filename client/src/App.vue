de5f9b8f821028222378e00f1b7e234a6d011682
Skyler Young <skylerdyoung95@gmail.com>
2020-10-02 20:55:42 -0400

    10/1/20 class

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,33 +1,22 @@
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <section class="section">
+    <div class="container">
+      <Nav />
+      <router-view/>
     </div>
-    <router-view/>
-    Welcome to New Paltz
-  </div>
+  </section>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Nav from "@/components/Nav";
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+    components: {
+        Nav
     }
-  }
 }
+</script>
+
+<style lang="scss">
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,24 @@
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome to our home page</h1>
+
+    <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+      
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
