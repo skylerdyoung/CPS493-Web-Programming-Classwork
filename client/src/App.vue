e471faa4c18abd753553b083a5e49a1f8709d76d
Skyler Young <skylerdyoung95@gmail.com>
2020-10-05 17:52:20 -0400

    10-5-20

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,15 @@
 
 <template>
-  <section class="section">
-    <div class="container">
+  <div class="">
       <Nav />
-      <router-view/>
-    </div>
-  </section>
+      <div class="container">
+          <router-view/>
+      </div>
+  </div>
 </template>
 
 <script>
 import Nav from "@/components/Nav";
-
 export default {
     components: {
         Nav
--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,19 +1,20 @@
+
 <template>
   
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar container is-fixed-top is-info" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
     <a class="navbar-item" href="https://bulma.io">
       <img src="../assets/logo.png" height="28">
     </a>
 
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" class="navbar-burger burger" :class="{ 'is-active': isActive }" @click="isActive = !isActive" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
+  <div id="navbarBasicExample" class="navbar-menu "  :class="{ 'is-active': isActive }">
     <div class="navbar-start">
       <router-link to="/"  class="navbar-item" >Home</router-link>
       <router-link to="/about"  class="navbar-item" >About</router-link>
@@ -44,12 +45,12 @@
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
-          <a class="button is-primary">
+          <a class="button">
             <strong>Sign up</strong>
           </a>
-          <a class="button is-light">
-            Log in
-          </a>
+            <router-link to="/login"  class="button is-light" >
+                Log in
+            </router-link>
         </div>
       </div>
     </div>
@@ -61,6 +62,11 @@
 
 <script>
 export default {
+  data: ()=> ({
+      isActive: false
+  }),
+  methods: {
+  }
 }
 </script>
 
