2d02463d23a1dcdabd93efa7c3ae49195de029fd
Skyler Young <skylerdyoung95@gmail.com>
2020-10-19 14:57:13 -0400

    error handling

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,7 +5,11 @@ const users = require('../models/users');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    throw { status: 501, message: "This is a fake error"}
     res.send(users.getAll() );
+})
+.get('/search', (req, res) => {
+    res.send( users.search(req.query.q) );
 })
 .post('/', (req, res) => {
     const newUser = users.create(req.query.name, req.query.age );
--- server/index.js
+++ server/index.js
@@ -7,11 +7,16 @@ const users = require('./controllers/users');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Hello Hudson Valley! You requested ' + req.url)
 })
 
 app.use('/users', users);
+
+app.use( (err, req, res, next)  => {
+  console.log(err);
+  res.status(err.status || 500).send( { message: err.message } )
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
