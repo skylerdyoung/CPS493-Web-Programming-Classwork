5d804e4e2d41bbd2e03c9221af70bc6c114ebf98
Skyler Young <skylerdyoung95@gmail.com>
2020-11-08 20:15:45 -0500

    recent

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const users = require('../models/users');
 
@@ -7,7 +5,7 @@ const router = express.Router();
 
 router
     .get('/', (req, res, next) => {
-        users.getAll().then(x=> res.send( x ) )
+        users.getAll().then(x=> res.send( x.map(user=> ({ ...user, Password: undefined}) ) ) )
         .catch(next);
     })
     .get('/:id', (req, res, next) => {
@@ -25,9 +23,50 @@ router
         .catch(next);
     })
     .post('/', (req, res, next) => {
-        users.add(req.query.name, req.query.age ).then(newUser => {
+        users.add(
+            req.body.FirstName,
+            req.body.LastName, 
+            req.body.DOB, 
+            req.body.Password, 
+            users.Types.USER, 
+        ).then(newUser => {
             res.send( newUser );
         }).catch(next)
     })
-
+    .post('/register', (req, res, next) => {
+        users.register(
+            req.body.FirstName,
+            req.body.LastName, 
+            req.body.DOB, 
+            req.body.Password, 
+            users.Types.USER,
+            req.body.Email 
+        ).then(newUser => {
+            res.send( { ...newUser, Password: undefined } );
+        }).catch(next)
+    })
+    .post('/login', (req, res, next) => {
+        users.login(
+            req.body.email,
+            req.body.password
+        ).then(newUser => {
+            res.send( { ...newUser, Password: undefined } );
+        }).catch(next)
+    })
+   .put('/:id', (req, res, next) => {
+        users.update( req.params.id,
+            req.body.FirstName,
+            req.body.LastName, 
+            req.body.DOB, 
+            req.body.Password, 
+            users.Types.USER, 
+        ).then(newUser => {
+            res.send( newUser );
+        }).catch(next)
+    })
+    .delete('/:id', (req, res, next) => {
+        users.remove(req.params.id).then(msg => {
+            res.send( msg );
+        }).catch(next)
+    })
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -4,9 +4,19 @@ require('dotenv').config();
 const users = require('./controllers/users');
 
 const app = express()
-const port = process.env.PORT  || 3000;
+const port = process.env.PORT || 3000;
 
 console.log(process.env.BEST_CLASS);
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  const arr = (req.headers.authorization || "").split(" ");
+  if(arr.length > 1 && arr[1] != null){
+      req.userId = +arr[1];
+  }
+  next();
+});
 
 app.get('/', (req, res, next) => {
   res.send('Hello Hudson Valley! You requested ' + req.url)
@@ -14,10 +24,10 @@ app.get('/', (req, res, next) => {
 
 app.use('/users', users);
 
-app.use( (err, req, res, next)  => {
-  console.log(err);
-  res.status(err.status || 500).send( { message: err.message } )
-})
+app.use( (err, req, res, next) =>{
+    console.log(err);
+    res.status(err.status || 500).send( { message: err.message } )
+} )
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
