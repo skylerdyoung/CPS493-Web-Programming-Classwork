52e18ba4e7a8683d608e639f4a7f1b630ec6955a
Skyler Young <skylerdyoung95@gmail.com>
2020-11-16 14:30:51 -0500

    recent

--- server/index.js
+++ server/index.js
@@ -1,15 +1,22 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 require('dotenv').config();
 
 const users = require('./controllers/users');
+const posts = require('./controllers/posts');
+const comments = require('./controllers/comments');
+const reactions = require('./controllers/reactions');
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 console.log(process.env.BEST_CLASS);
 
+//  Middleware
 app.use(express.json());
+app.use(express.static( __dirname + '/../docs/'))
 
+//  Authentication
 app.use(function(req, res, next) {
   const arr = (req.headers.authorization || "").split(" ");
   if(arr.length > 1 && arr[1] != null){
@@ -18,17 +25,30 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/', (req, res, next) => {
+//  API
+app.get('/hello', (req, res, next) => {
   res.send('Hello Hudson Valley! You requested ' + req.url)
 })
 
 app.use('/users', users);
+app.use('/posts', posts);
+app.use('/comments', comments);
+app.use('/reactions', reactions);
+
+app.get('*', (req, res, next) => {
+    const filename = path.join(__dirname, '/../docs/index.html');
+    console.log(filename);
+    res.sendFile( filename );
+})
 
 app.use( (err, req, res, next) =>{
     console.log(err);
     res.status(err.status || 500).send( { message: err.message } )
 } )
 
+
+
+//  Init
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
